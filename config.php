4f14be5069ada90af39af3f7bd5391a7c0ade336
Abd Shomad <abd.shomad@gmail.com>
2012-09-07 20:35:34 +0700

    php files

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php /* Configuration file for LionWiki. */
+$WIKI_TITLE = 'Indeks Kamus Al-Quran Kata per Kata'; // name of the site
+
+// SHA1 hash of password. If empty (or commented out), no password is required
+// $PASSWORD = sha1("my_password");
+
+$TEMPLATE = 'templates/wikiss-ar.html'; // presentation template
+
+// if true, you need to fill password for reading pages too
+// before setting to true, read http://lionwiki.0o.cz/index.php?page=UserGuide%3A+How+to+use+PROTECTED_READ
+$PROTECTED_READ = false;
+
+$NO_HTML = true; // XSS protection
+
+$LANG = 'id'; 
+
+$START_PAGE = 'index'; 
---
+++ lang/id.php
@@ -0,0 +1,31 @@
+<?php
+/*
+	This file is actually never used but provides easier access to translators as they will
+	probably translate from english.
+*/
+$T_HOME = 'Halaman Utama';
+$T_SYNTAX = 'Sintaks';
+$T_DONE = 'Simpan perubahan';
+$T_DISCARD_CHANGES = 'Buang perubahan';
+$T_PREVIEW = 'Pratinjau';
+$T_SEARCH = 'Cari';
+$T_SEARCH_RESULTS = 'Hasil pencarian';
+$T_LIST_OF_ALL_PAGES = 'Daftar semua halaman';
+$T_RECENT_CHANGES = 'Perubahan terbaru';
+$T_LAST_CHANGED = 'Perubahan terakhir';
+$T_HISTORY = 'Sejarah';
+$T_RESTORE = 'Pulihkan';
+$T_REV_DIFF = '<b>Perbedaan antara revisi {REVISION1} dan {REVISION2}.</b>';
+$T_REVISION = "'''Revisi ini berasal dari {TIME}. Anda bisa {RESTORE} revisi ini.'''\n\n";
+$T_PASSWORD = 'Password';
+$T_EDIT = 'Edit';
+$T_EDIT_SUMMARY = 'Ringkasan perubahan';
+$T_EDIT_CONFLICT = 'Edit konflik: Seseorang menyimpan halaman ini setelah Anda mulai mengedit. Lihat {DIFF} terakhir sebelum menyimpan perubahan Anda.';
+$T_SHOW_SOURCE = 'Tampilkan sumber';
+$T_SHOW_PAGE = 'Tampilkan halaman';
+$T_ERASE_COOKIE = 'Hapus cookies';
+$T_MOVE_TEXT = 'Nama baru';
+$T_DIFF = 'perbedaan';
+$T_CREATE_PAGE = 'Buat halaman';
+$T_PROTECTED_READ = 'Anda perlu memasukkan password untuk melihat isi situs: ';
+$T_WRONG_PASSWORD = 'Password salah.';
